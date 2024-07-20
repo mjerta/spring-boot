1d624ea5a64bea63c3e96728bf61335210642268
Maarten Postma <maarten.postma1000@gmail.com>
2024-07-21 01:36:11 +0200

    Add some crud functionalitys

--- src/main/java/com/example/spring_boot/HelloController.java
+++ src/main/java/com/example/spring_boot/HelloController.java
@@ -1,14 +1,58 @@
 package com.example.spring_boot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 public class HelloController {
+
+  private final PhotosService photosService;
+
+  public HelloController(@Autowired PhotosService photosService) {
+    this.photosService = photosService;
+  }
 
   @GetMapping("/")
   public String index() {
     return "Greetings from Spring eot!";
   }
+
+  @GetMapping("/photos")
+  public Collection<Photo> getPhotos() {
+    return photosService.get();
+  }
+
+  @GetMapping("/photos/{id}")
+  public Photo getPhoto(@PathVariable String id) {
+    Photo photo = photosService.get(id);
+    if(photo == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    return photo;
+  }
+
+
+  @DeleteMapping("/photos/{id}")
+  public void delete(@PathVariable String id) {
+    Photo photo = photosService.remove(id);
+    if(photo == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @PostMapping("/photos")
+  public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+    Photo photo = photosService.save(file.getOriginalFilename(), file.getContentType(),  file.getBytes());
+    return photo;
+  }
+
+
 
 }
---
+++ src/main/java/com/example/spring_boot/Photo.java
@@ -0,0 +1,55 @@
+package com.example.spring_boot;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotEmpty;
+
+public class Photo {
+  private String id;
+  @NotEmpty
+  private String fileName;
+
+  @JsonIgnore
+  private byte[] data;
+
+  private String contentType;
+
+  public Photo() {
+  }
+
+  public Photo(String id, String fileName) {
+    this.id = id;
+    this.fileName = fileName;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
+  }
+
+  public byte[] getData() {
+    return data;
+  }
+
+  public void setData(byte[] data) {
+    this.data = data;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+}
